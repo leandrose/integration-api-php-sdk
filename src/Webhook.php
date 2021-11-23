d050aa9a180196fc100f4205fecd19babc12a66a
Leandro Senni <ltsujiguchi@gmail.com>
2021-11-23 14:55:54 -0300

    Webhook: fix, remove method create() (loop)

--- src/Webhook.php
+++ src/Webhook.php
@@ -1,17 +1,12 @@
 <?php
+
 namespace TamoJuno;
 
-use TamoJuno\Resource;
-
-class Webhook extends Resource {
+class Webhook extends Resource
+{
 
     public function endpoint(): string
     {
         return 'notifications/webhooks';
-    }
-
-    public function create(array $form_params = [])
-    {
-        return $this->create($form_params);
     }
 }
